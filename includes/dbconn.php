138797755d102e4c177471d2ccad20745f69f4bf
Sumit Harishchandra Mhaske <sumitmhaske435@gmail.com>
2025-02-01 16:37:19 +0530

    connection

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -1,7 +1,7 @@
 <?php
-    $dbuser="root";
-    $dbpass="";
-    $host="localhost";
-    $db="hostelmsphp";
+    $dbuser="uh24mqaavomifklq";
+    $dbpass="i4cEGDLglCox9Yr4BUYP";
+    $host="bypt420ycdrlcrvehad9-mysql.services.clever-cloud.com";
+    $db="bypt420ycdrlcrvehad9";
     $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
 ?>
